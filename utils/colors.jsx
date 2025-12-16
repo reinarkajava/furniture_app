38e7ff7545027c0249f9897d91858a0d675050ff
reinarkajava <reinar.kajava@voco.ee>
2025-12-16 21:13:30 +0200

    Removing errors

--- utils/colors.jsx
+++ utils/colors.jsx
@@ -3,4 +3,8 @@ export const colors = {
   orange: "#fca34d",
   white: "#ffffff",
   black: "#303030",
+  grey: "#8d9bb5",
+  lightgray: "#dadada",
+  googleGray: "#3f4a59",
+  textGray: "#606060"
 }; 
